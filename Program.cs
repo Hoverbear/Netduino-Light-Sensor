e58261e673fa77eb191e06c625c0e923297a5919
Andrew Hobden <andy.hobden@gmail.com>
2011-12-07 16:24:48 -0800

    First commit.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+using Light_Sensor;
+
+namespace Light_Sensor
+{
+    public class Program
+    {
+        // Sensor
+        static AnalogInput Sensor = new AnalogInput(Pins.GPIO_PIN_A0);
+        static int Range = 10;
+
+        // Sleep time
+        static int TransmissionGap = 500;
+
+        // Display
+        static OutputPort[] LEDArray = {
+                new OutputPort(Pins.GPIO_PIN_D0, false), 
+                new OutputPort(Pins.GPIO_PIN_D1, false), 
+                new OutputPort(Pins.GPIO_PIN_D2, false), 
+                new OutputPort(Pins.GPIO_PIN_D3, false), 
+                new OutputPort(Pins.GPIO_PIN_D4, false), 
+                new OutputPort(Pins.GPIO_PIN_D5, false), 
+                new OutputPort(Pins.GPIO_PIN_D6, false),
+                new OutputPort(Pins.GPIO_PIN_D7, false)
+            };
+        static SegmentLED LED = new SegmentLED(LEDArray);
+
+
+        public static void Main()
+        {
+            // Initialize the value.
+            int Value = 0;
+            // Setup the range.
+            Sensor.SetRange(0, Range);
+
+            // Initialization test.
+            for (int i = 0; i <= 10; i++)
+            {
+                LED.Display(i);
+                Thread.Sleep(2000);
+            }
+
+            // Read, Print, Transmit, Sleep loop.
+            while (true)
+            {
+                Value = Sensor.Read(); // So we only read once.
+                Debug.Print("Value to Transmit: " + Value);
+                LED.Display(Value);
+                Thread.Sleep(TransmissionGap); // Sleep for a short time.
+            }
+        }
+    }
+}
---
+++ SegmentLED.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+using Light_Sensor;
+
+namespace Light_Sensor
+{
+    class SegmentLED
+    {
+        private OutputPort[] LEDArray;
+        private bool[] state = new bool[8];
+
+        public SegmentLED(OutputPort[] LEDArray)
+        {
+            this.LEDArray = LEDArray;
+        }
+
+        internal void Display(int Value)
+        {
+            foreach (OutputPort port in LEDArray)
+            {
+                port.Write(false);
+            }
+            switch (Value)
+            {
+                case 1:
+                    LEDArray[1].Write(true); // 2
+                    LEDArray[3].Write(true); // 7
+                    break;
+                case 2:
+                    LEDArray[0].Write(true); // 1
+                    LEDArray[1].Write(true); // 2
+                    LEDArray[5].Write(true); // 9
+                    LEDArray[6].Write(true); // 13
+                    LEDArray[7].Write(true); // 14
+                    break;
+                case 3:
+                    LEDArray[0].Write(true); // 1
+                    LEDArray[1].Write(true); // 2
+                    LEDArray[5].Write(true); // 9
+                    LEDArray[3].Write(true); // 7
+                    LEDArray[7].Write(true); // 14
+                    break;
+                case 4:
+                    LEDArray[4].Write(true); // 8
+                    LEDArray[5].Write(true); // 9
+                    LEDArray[1].Write(true); // 2
+                    LEDArray[3].Write(true); // 7
+                    break;
+                case 5:
+                    LEDArray[0].Write(true); // 1
+                    LEDArray[4].Write(true); // 8
+                    LEDArray[5].Write(true); // 9
+                    LEDArray[3].Write(true); // 7
+                    LEDArray[7].Write(true); // 14
+                    break;
+                case 6:
+                    LEDArray[0].Write(true); // 1
+                    LEDArray[4].Write(true); // 8
+                    LEDArray[5].Write(true); // 9
+                    LEDArray[6].Write(true); // 13
+                    LEDArray[3].Write(true); // 7
+                    LEDArray[7].Write(true); // 14
+                    break;
+                case 7:
+                    LEDArray[0].Write(true); // 1
+                    LEDArray[1].Write(true); // 2
+                    LEDArray[3].Write(true); // 7
+                    break;
+                case 8:
+                    LEDArray[0].Write(true); // 1
+                    LEDArray[4].Write(true); // 8
+                    LEDArray[1].Write(true); // 2
+                    LEDArray[5].Write(true); // 9
+                    LEDArray[6].Write(true); // 13
+                    LEDArray[3].Write(true); // 7
+                    LEDArray[7].Write(true); // 14
+                    break;
+                case 9:
+                    LEDArray[0].Write(true); // 1
+                    LEDArray[4].Write(true); // 8
+                    LEDArray[1].Write(true); // 2
+                    LEDArray[5].Write(true); // 9
+                    LEDArray[3].Write(true); // 7
+                    LEDArray[7].Write(true); // 14
+                    break;
+                default:
+                    LEDArray[2].Write(true); // 6
+                    break;
+            }
+        }
+    }
+}
